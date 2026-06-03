f74ca224ed9209cac9794696d167ae768fd8307f
conixann <mil.elgueta@duocuc.cl>
2026-06-03 18:11:55 -0400

    If agregados y control de errores actualizado (final)

--- TreaGit.py
+++ TreaGit.py
@@ -1,10 +1,10 @@
 total_discount=0
 while True:
     product=input("Ingrese nombre de el producto: ").strip().title()
-    if len(product) >3 and product.isalpha():
+    if len(product) >3:
         break
     else:
-        print("Error, el producto contiene muy pocos caracteres o ingresaste números, asegurate de ingresar solo letras")
+        print("Error, el producto contiene muy pocos caracteres. ")
 while True:
     try:        
         price=int(input("Ingrese el precio de el producto: "))
@@ -17,12 +17,20 @@ while True:
 while True:
     try:
         discount=int(input("Ingrese el descuento al producto:"))
-        if discount > 0:
+        if discount >= 50:
+                warning=input("El descuento es igual o mayor a el 50% ¿Estas seguro que lo deseas aplicar? (S/N): ").upper().strip()
+                if warning == "S":
+                    break
+                else: 
+                    continue
+        elif discount >0:
             break
         else: 
             print("Error, el descuento debe ser mayor que 0")
     except ValueError:
         print("Error, ingresaste caracteres inválidos, ingresa solo números positivos")
+
+total_discount= int(price * (1 - discount/100))
 
 
 print(f"Producto: {product}")
