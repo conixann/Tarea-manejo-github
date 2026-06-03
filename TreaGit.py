400b6ebb6a4462919a84a881515e2bc8312d354e
conixann <mil.elgueta@duocuc.cl>
2026-06-03 17:40:15 -0400

    Entrada y salida de datos (inicial)

---
+++ TreaGit.py
@@ -0,0 +1,10 @@
+total_discount=0
+
+product=input("Ingrese nombre de el producto: ")
+price=int(input("Ingrese el precio de el producto: "))
+discount=int(input("Ingrese el descuento al producto:"))
+
+print(f"Producto: {product}")
+print(f"Precio original: {price}")
+print(f"Descuento: {discount}")
+print(f"Precio final: {total_discount} ")
