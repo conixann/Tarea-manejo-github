9abc3d0ebd1e378f8abd301988b608c6eaa0ee71
conixann <mil.elgueta@duocuc.cl>
2026-06-03 21:44:52 -0400

    Validaciones y entrada de datos modificada (final)

--- TareaGit3.py
+++ TareaGit3.py
@@ -1,22 +1,53 @@
 import os,time
 
 saldo=100000
-
+os.system('cls')
 while True:
     print("Cajero Automático")
     print("1. Consultar saldo")
     print("2. Depositar dinero")
     print("3. Girar dinero")
-    print("4. Salir")
+    print("4. Salir") 
     opc=input("Ingrese opción: ").strip()
+    
     if opc=="1":
+        os.system('cls')
         print(f"Saldo disponible: {saldo}")
+        time.sleep(3)
     elif opc=="2":
-        pass
+        os.system('cls')
+        while True:
+            try:
+                deposito=int(input("¿Cuanto dinero desea ingresar?:"))
+                if deposito > 0:
+                    saldo += deposito
+                    print(f"Saldo actual: {saldo}")
+                    time.sleep(3)
+                    break
+            except ValueError:
+                print("ERROR! Ingrese un número entero positivo!")
     elif opc=="3":
-        pass
+        os.system('cls')
+        while True:
+            try:
+                giro=int(input("¿Cuanto dinero desea retirar?:"))
+                if giro > saldo:
+                    print("Error, excede el máximo de saldo disponible.")
+                elif giro > 0:
+                    saldo -= giro
+                    print(f"Saldo actual: {saldo}")
+                    time.sleep(3)
+                    break
+    
+            except ValueError:
+                print("ERROR! ingresa un número entero positivo")
+                time.sleep(3)
     elif opc=="4":
-        print("Gracias por haber usado nuestros servicios, adiós,")
-        exit
+        os.system('cls')
+        print("Gracias por haber usado nuestros servicios, adiós.")
+        time.sleep(3)
+        
+        break
     else:
-        pass
+        print("Opción no válida. Intente nuevamente.")
+        time.sleep(2)
